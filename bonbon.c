8c6dd673fe49520f5e9c03628cfe956662f769ee
jeka-js <jeka7js@gmail.com>
2013-01-12 01:07:04 +0200

    Update

--- bonbon.c
+++ bonbon.c
@@ -4,6 +4,13 @@
 #include <gtk/gtk.h>
 #include <stdlib.h>
 
+/* Global variables */
+
+GtkWidget * statusbar;
+int is_connected = 0;
+
+/* Entry point */
+
 int main(int argc, char * argv[])
 {
     /* GTK initialization */
@@ -15,16 +22,19 @@ int main(int argc, char * argv[])
     GtkBuilder * builder = gtk_builder_new();
     GError * error_msg = NULL;
 
-    if (!gtk_builder_add_from_file(builder, GLADE_FILE, &error_msg))
+    if (!gtk_builder_add_from_file(builder, GLADE_FILE_NAME, &error_msg))
     {
         g_warning("%s\n", error_msg->message);
         return EXIT_FAILURE;
     }
 
     GtkWidget * main_window = GTK_WIDGET(gtk_builder_get_object(builder, MAIN_WINDOW_NAME));
+    statusbar = GTK_WIDGET(gtk_builder_get_object(builder, STATUSBAR_NAME));
+    GtkWidget * box_Result = GTK_WIDGET(gtk_builder_get_object(builder, BOX_RESULT_NAME));
 
     /* Starting */
 
+    gtk_statusbar_push(GTK_STATUSBAR(statusbar), 0, READY_MESSAGE);
     gtk_builder_connect_signals(builder, NULL);
     g_object_unref(G_OBJECT(builder));
     gtk_widget_show_all(main_window);
--- bonbon.h
+++ bonbon.h
@@ -1,9 +1,21 @@
 #ifndef BONBON_H_INCLUDED
 #define BONBON_H_INCLUDED
 
-#define GLADE_FILE              "data/bonbon.glade"
+/* Files */
+
+#define GLADE_FILE_NAME         "./data/bonbon.glade"
+
+/* Objects (in XML) */
 
 #define MAIN_WINDOW_NAME        "main_window"
+#define STATUSBAR_NAME          "statusbar"
+#define BOX_RESULT_NAME         "box_Result"
+
+/* Statusbar messages */
+
+#define READY_MESSAGE           "Ready!"
+#define CONNECTED_MESSAGE       "Connected."
+#define DISCONNECTED_MESSAGE    "Disconnected!"
 
 #endif // BONBON_H_INCLUDED
 
