45212a9d98c5ab75e03d40c5d77ef8a75821ed26
jeka-js <jeka7js@gmail.com>
2013-01-13 16:30:18 +0200

    Page keyboard skeleton

---
+++ page_keyboard.c
@@ -0,0 +1,8 @@
+#include "page_keyboard.h"
+#include "funcs.h"
+
+int page_keyboard_bind(GtkBuilder * builder)
+{
+    return 0;
+}
+
---
+++ page_keyboard.h
@@ -0,0 +1,9 @@
+#ifndef PAGE_SHELL_H_INCLUDED
+#define PAGE_SHELL_H_INCLUDED
+
+#include <gtk/gtk.h>
+
+int page_keyboard_bind(GtkBuilder *);
+
+#endif // PAGE_SHELL_H_INCLUDED
+
